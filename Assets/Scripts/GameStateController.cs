80650eada79d1cb6e841e09980fc1570f276c20a
Spencer <sssmoot@ucsc.edu>
2016-12-13 00:43:24 -0800

    Build sent to E setting it down.

--- Assets/Scripts/GameStateController.cs
+++ Assets/Scripts/GameStateController.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class GameStateController : MonoBehaviour {
     public Animator animator;
     int placement;
+
+    GameObject[] paths;
 	// Use this for initialization
 	void Start () {
         //animator = GetComponent<Animator>();
-        
+        paths = GameObject.FindGameObjectsWithTag("Path");
         placement = 0;
+        
 	}
 	
 	// Update is called once per frame
@@ -21,7 +24,27 @@ public class GameStateController : MonoBehaviour {
        // print("here");
        // print(animator.GetInteger("Changeit"));
         placement += 1;
+        TurnOnPath();
         animator.SetInteger("Changeit",placement);
        // print(animator.GetInteger("Changeit"));
+    }
+    public void TurnOnPath()
+    {
+        for(int i=0; i < paths.Length; i++)
+        {
+            if (paths[i].name[0] == placement.ToString()[0])
+            {
+                print("here");
+                MeshRenderer[] turnon;
+                turnon = paths[i].GetComponentsInChildren<MeshRenderer>();
+                
+                foreach(MeshRenderer path in turnon)
+                {
+                    print(path.name);
+                    path.enabled = true;
+                }
+
+            }
+        }
     }
 }
